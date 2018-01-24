0233b3a506e503013212b45d35c0d260dbee35f4
yunfeihe <yunfei.he@qq.com>
2018-01-24 18:48:21 +0800

    .

---
+++ spider-for-music163/spider.py
@@ -0,0 +1,19 @@
+import requests, lxml
+from bs4 import BeautifulSoup 
+
+def get_response_from_url(url):
+    headers = {
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+        "Accept-Encoding":"gzip, deflate",
+        "Accept-Language":"zh-CN,zh;q=0.9",
+        "Connection":"keep-alive",
+        "Host":"music.163.com",
+        "Referer":"http://music.163.com/",
+        "Upgrade-Insecure-Requests":"1",
+        "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36",
+         
+         }
+    response = requests.post(url, headers=headers)
+    return response
+
+test = get_response_from_url('http://music.163.com/#/user/home?id=1512211')
