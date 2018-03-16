9e058b12a363804bdf73348aea179057834efb2f
YunfeiHe <yunfei.he@qq.com>
2018-03-16 22:08:25 +0800

    .

--- temp/1/js/1.js
+++ temp/1/js/1.js
@@ -1,5 +1,18 @@
 var main = function(){
     log("start 1 main");
-}
+    // displaySlideShowByIndex(0);
+
+    setTimeout(displaySlideShowByIndex(1), 5000);
+};
+
+var displaySlideShowByIndex = function(index){
+    //索引从零开始
+    log("changeTo", index);
+    var images = sa("#slide-show li");
+    for(var i=0; i<images.length; i++){
+        images[i].style.transform ="translate(-" +  100 * index + "%)" ;
+    }
+    
+};
 
 main();
--- temp/1/js/utils.js
+++ temp/1/js/utils.js
@@ -9,3 +9,8 @@ var s = function(selector){
 var sa = function(selector){
     return document.querySelectorAll(selector);
 }
+
+// var addClass = function(obj, className){
+//     obj.classList.addClass(className);
+// };
+
