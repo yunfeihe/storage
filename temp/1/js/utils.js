ac2397eabe30423bdd6a6853a190d71d5473ea84
YunfeiHe <yunfei.he@qq.com>
2018-03-17 18:41:05 +0800

    .

--- temp/1/js/utils.js
+++ temp/1/js/utils.js
@@ -4,11 +4,11 @@ var log = function(){
 
 var s = function(selector){
     return document.querySelector(selector);
-}
+};
 
 var sa = function(selector){
     return document.querySelectorAll(selector);
-}
+};
 
 // var addClass = function(obj, className){
 //     obj.classList.addClass(className);
